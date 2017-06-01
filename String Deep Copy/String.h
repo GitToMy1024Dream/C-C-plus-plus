3a985ceb880b721fbe4b49e7c576028f6694946c
GitToMy1024Dream <postmaster@1024do.com>
2017-06-01 08:30:31 +0800

    String类的深拷贝

---
+++ String Deep Copy/String.h
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <Windows.h>
+#include <string.h>
+#pragma warning (disable:4996)
+using namespace std;
+class String
+{
+public:
+	String(const char *pStr = "")
+	{
+		if(pStr == NULL)
+		{
+			_pStr = new char[1];
+			*_pStr = '\0';
+		}
+		else
+		{
+			_pStr = new char[strlen(pStr) + 1];
+			strcpy(_pStr,pStr);
+		}
+	}
+	String(const String& s)
+	{
+		_pStr = new char[strlen(s._pStr) + 1];
+		strcpy(_pStr,s._pStr);
+	}
+	~String()
+	{
+		if(_pStr)
+		{
+			delete[] _pStr;
+			_pStr = NULL;
+		}
+	}
+	size_t Size()const;//字符串的大小
+	size_t Lengh()const;//字符串的长度
+	char& operator[](const size_t index);//下标界定符
+	const char& operator[](size_t index)const;
+	bool operator>(const String& s);
+	bool operator<(const String& s);
+	bool operator==(const String& s);
+	bool operator!=(const String& s);
+	void Copy(const String& s);
+	String operator+(const String& s);
+	bool strstr(const String& s);
+	String& operator=(const String& s);
+	String& operator+=(const String& s);
+	friend ostream& operator<<(ostream& _cout,const String& s);
+private:
+	char *_pStr;
+};
